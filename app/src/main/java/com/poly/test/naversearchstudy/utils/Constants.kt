00e1ae4e4bc065f0fe147c60beba1343db7e6940
Administrator <skwk55555@naver.com>
2021-01-23 12:48:45 +0900

    UI 업데이트

--- app/src/main/java/com/poly/test/naversearchstudy/utils/Constants.kt
+++ app/src/main/java/com/poly/test/naversearchstudy/utils/Constants.kt
@@ -7,8 +7,8 @@ object Constants {
 object API {
 
     const val BASE_URL: String = "https://openapi.naver.com/"
-    const val CLIENT_ID: String = "gSZg2bvyDyX5ZLYwdkjl"
-    const val CLIENT_KEY: String = "qAMJelGnEz"
+    const val CLIENT_ID: String = "your Client Id"
+    const val CLIENT_KEY: String = "your Client Key"
     const val SEARCH_BOOK: String ="v1/search/book?query"
 
 
